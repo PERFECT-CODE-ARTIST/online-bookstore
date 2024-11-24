0d679eca819ff4466eb47466977dd7e850fcd289
ckstnr99 <ckstnr99@naver.com>
2024-11-24 11:30:18 +0900

    2024.11.24. 11:28 review entity anotation update & review service classify update

--- src/main/java/com/bookstore/online/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/bookstore/online/domain/review/repository/ReviewRepository.java
@@ -6,7 +6,6 @@ import com.bookstore.online.domain.review.entity.ReviewEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer> {
